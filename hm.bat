123e7c288a7c6ee543ec1943d9e52ee0bf050b28
NILEG2 <NILEhelptogo@Outlook.com>
2024-11-30 19:54:22 +0100

    this is the hacker type!

---
+++ hm.bat
@@ -0,0 +1,11 @@
+@echo off 
+:a
+title hacking.
+title(        )
+title hacking..
+title (        )
+title hacking...
+echo ...
+color 02
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto a
