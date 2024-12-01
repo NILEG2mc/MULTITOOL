6e067f8603709ec42b324c1eb1dfbcbe71052df7
NILEG2 <NILEhelptogo@Outlook.com>
2024-12-01 11:36:17 +0100

    USELESS RATE FILE WERE YOU GO TO THE PAGE !

---
+++ RATE.bat
@@ -0,0 +1,48 @@
+@echo off
+title N/I/L/E/G/2 TOOL(RATE THIS PROGRAMM!)
+chcp 65001 >nul
+cd MODS
+color 5
+:start
+call :banner
+
+:menu
+for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
+echo.
+echo.
+echo [38;2;255;255;0m        ╔═(1) 1 [0m  
+echo [38;2;255;255;0m        ║[0m  
+echo [38;2;255;255;0m        ╠══(2) 2[0m  
+echo [38;2;255;255;0m        ║[0m  
+echo [38;2;255;255;0m        ╠═══(3) 3[0m  
+echo [38;2;255;255;0m        ║[0m  4
+echo [38;2;255;255;0m        ╚╦═══(5) 5 [0m  
+echo [38;2;255;255;0m         ║[0m  
+set /p input=.%BS% [38;2;255;255;0m        ╚══════^>[0m  
+if /I %input% EQU 1 start https://github.com/NILEG2mc/MULTITOOL                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+if /I %input% EQU 2 start https://github.com/NILEG2mc/MULTITOOL
+if /I %input% EQU 3 start https://github.com/NILEG2mc/MULTITOOL
+if /I %input% EQU 4 start https://github.com/NILEG2mc/MULTITOOL
+if /I %input% EQU 5 start https://github.com/NILEG2mc/MULTITOOL
+cls
+goto start
+
+:banner
+echo.
+echo.
+echo N)n   nn      /)? I)iiii      /)? L)            /)? E)eeeeee      /)?   G)gggg      /)?  2)AAA  
+echo N)nn  nn     /)?    I)       /)?  L)           /)?  E)           /)?   G)          /)?  2)   AA 
+echo N) nn nn    /)?     I)      /)?   L)          /)?   E)eeeee     /)?   G)  ggg     /)?       2)  
+echo N)  nnnn   /)?      I)     /)?    L)         /)?    E)         /)?    G)    gg   /)?       2)   
+echo N)   nnn  /)?       I)    /)?     L)        /)?     E)        /)?      G)   gg  /)?       2)    
+echo N)    nn /)?      I)iiii /)?      L)llllll /)?      E)eeeeee /)?        G)ggg  /)?      2)AAAAA 
+                                                                                                
+                                                                                                
+echo T)tttttt  O)oooo   O)oooo  L)          () T)tttttt  M)mm mmm  ))                                
+  echo T)    O)    oo O)    oo L)        ()      T)    M)  mm  mm   ))                              
+  echo T)    O)    oo O)    oo L)       ()       T)    M)  mm  mm    ))                             
+  echo T)    O)    oo O)    oo L)       ()       T)    M)  mm  mm    ))                             
+  echo T)    O)    oo O)    oo L)        ()      T)    M)      mm   ))                              
+  echo T)     O)oooo   O)oooo  L)llllll    ()    T)    M)      mm ))                                
+   
